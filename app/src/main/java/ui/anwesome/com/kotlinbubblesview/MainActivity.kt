11c6845dbfb3b4ff6e29e47486dbbc248b60a531
Anwesh43 <anweshthecool0@gmail.com>
2017-12-28 01:04:47 +0530

    using bubbleview in activity

--- app/src/main/java/ui/anwesome/com/kotlinbubblesview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbubblesview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinbubblesview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.bubblesview.BubblesView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BubblesView.create(this)
     }
 }
